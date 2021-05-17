12792776738e673ef3ef4c7d7f0c578caf53af83
neeraj15022001 <gneeraj32595@gmail.com>
2021-05-18 01:29:00 +0530

    final changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./App.css";
-import Card from "./components/Card"
-import InputContainer from "./components/inputContainer"
-import PageHeading from "./components/PageHeading"
+import Card from "./components/Card";
+import InputContainer from "./components/inputContainer";
+import PageHeading from "./components/PageHeading";
 
 function App() {
-  const [tasks, setTasks] = useState([])
-  console.log(tasks)
-  function addTask({description}) {
-    setTasks(tasks => [...tasks,description])
+  const [tasks, setTasks] = useState([]);
+  function addTask({ description }) {
+    setTasks((tasks) => [...tasks, description]);
+  }
+  function removeTask(content) {
+    var array = [...tasks]; // make a separate copy of the array
+    var index = array.indexOf(content);
+    if (index !== -1) {
+      array.splice(index, 1);
+      setTasks(() => array);
+    }
   }
   return (
     <div className="App bg-dark">
       <PageHeading />
       <InputContainer addTask={addTask} />
       <div className="task-container container-fluid d-flex flex-column align-items-center justify-content-center">
-        {tasks.map((item,index) => (
-          <Card description={item} key={index} />
+        {tasks.map((item, index) => (
+          <Card description={item} key={index} removeTask={removeTask} />
         ))}
       </div>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 
-function Card({description}) {
+function Card({description, removeTask}) {
+  function remove() {
+    removeTask(description)
+  }
   return (
     <div className="card text-dark bg-light mb-3 w-100">
       <div className="card-body d-flex align-items-center justify-content-between">
         <p className="card-text">{description}</p>
-        <button className="rounded-circle text-white btn btn-danger ms-4">
+        <button className="rounded-circle text-white btn btn-danger ms-4" onClick={remove}>
           <i className="bi bi-dash"></i>
         </button>
       </div>
